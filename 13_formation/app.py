55bfc10e0f4d3df63b6a04c7d5589980d6e1e7c0
Joyce Liao <jycqliao@gmail.com>
2018-09-27 19:34:44 -0400

    modified k13

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -1,27 +1,28 @@
-#Team
+#Joyce Liao
 #SoftDev1 pd8
-#K
-#2018
+#K13 -- Echo echo echo
+#2018-09-27
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    print(app)
-    return render_template('template.html')
+def welcome():
+    return render_template('template.html',
+                            title = "Welcome",
+                            home_page = True)
 
-@app.route('/req')
-def req():
-    print(app)
-    print(request)
-    print(request.args)
-    print(request.headers)
-    print(request.args['username'])
-    return render_template('
+@app.route('/welcome')
+def greetings():
+    return render_template('template.html',
+                           title = "Welcome",
+                           username = request.args['username'],
+                           req_method = request.method)
 
 if (__name__) == ("__main__"):
     app.debug = True
     app.run()
+
+
 
