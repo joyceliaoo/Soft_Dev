7d251908400743ca0fc0648995f620aaf45f5581
Joyce Liao <jycqliao@gmail.com>
2018-11-13 21:17:07 -0500

    k24 fieles

---
+++ 24_rest/app.py
@@ -0,0 +1,25 @@
+#Joyce Liao
+#SoftDev1 pd8
+#K24 -- A RESTful Journey Skyward
+#2018-11-14
+
+from flask import Flask, render_template
+import urllib.request
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	obj = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?date=2018-11-12&api_key=03XZMhIrwzaAFyGNInqzU0M9uCvttt56biaVYJEk')
+	byte_data = obj.read() #reads data from httpResponse object
+	data = byte_data.decode("utf-8") #converts bytes into str
+	data_dict = json.loads(data) #converts json format data into dictionary
+	#print(data_dict)
+	#print(data_dict['url'])
+	return render_template("index.html", pic=data_dict['url'], description=data_dict['explanation'])
+
+if (__name__ == "__main__"):
+	app.debug = True
+	app.run()
+
